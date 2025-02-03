19a9cfa0b1aa1ffb8f3e1eb07f550cfa6db4b7e6
Landon Davidson <accounts@landond.tech>
2025-02-03 14:03:17 -0800

    Add: findNextKey

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinarySearchTree<K extends Comparable<K>, V> implements OrderedDeletelessDictionary<K,V> {
 
@@ -48,8 +49,51 @@ public class BinarySearchTree<K extends Comparable<K>, V> implements OrderedDele
     // Returns null if the given key is greater than or equal to the largest
     // key present
     public K findNextKey(K key){
-        // TODO
-        return null;
+        // Stack is used to traverse back up the tree and find the smallest key larger than the given one
+        Stack<TreeNode<K, V>> stack = new Stack<>();
+        TreeNode<K, V> cur_node = root;
+        // Set to false if we take a left (found a key larger than the given one)
+        boolean isMax = true;
+        // Loop until we find the given key
+        while (cur_node.key != key) {
+            // If current node doesn't have children then key doesn't exist
+            if (cur_node.height == 0) {
+                return null;
+            }
+            // Add the current node (our next node's parent) to the stack
+            stack.add(cur_node);
+            // If key is less than current node's key go left
+            if (key.compareTo(cur_node.key) < 0) {
+                cur_node = cur_node.left;
+                isMax = false;
+            // Else if key is greater than or equal to current node's key go right
+            } else {
+                cur_node = cur_node.right;
+            }
+        }
+        // If we only took right turns than the given key is the max and there isn't a next key
+        if (isMax) {
+            return null;
+        }
+        // If the given key has a right child then we go right and repeatedly left
+        // then return the key of the first node without a left child
+        // (the smallest of the nodes bigger than the given key)
+        if (cur_node.right != null) {
+            cur_node = cur_node.right;
+            while (cur_node.left != null) {
+                cur_node = cur_node.left;
+            }
+            return cur_node.key;
+        }
+        // If the given key doesn't have a right child then go up until we find a parent with a larger key
+        // The parent is guaranteed to be the next key since anything to the right is going to be bigger
+        while (!stack.isEmpty()) {
+            cur_node = stack.pop();
+            if (cur_node.key.compareTo(key) > 0) {
+                return cur_node.key;
+            }
+        }
+        return cur_node.key;
     }
 
     // Returns the largest key which is less than the given key
